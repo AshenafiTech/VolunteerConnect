b89f7f0917aa25e86189a30980aefe94367fc40b
Blen Debebe <blendebebe24@gmail.com>
2025-05-10 08:31:04 +0300

    Merge pull request

--- frontend/jetpack_compose/app/src/main/java/com/mobile/volunteerconnect/data/api/ApiService.kt
+++ frontend/jetpack_compose/app/src/main/java/com/mobile/volunteerconnect/data/api/ApiService.kt
@@ -1,15 +1,21 @@
 package com.mobile.volunteerconnect.data.api
 
+import com.mobile.volunteerconnect.data.model.ApplicationResponse
 import com.mobile.volunteerconnect.data.model.LoginRequest
 import com.mobile.volunteerconnect.data.model.LoginResponse
 import com.mobile.volunteerconnect.data.model.SignupRequest
 import com.mobile.volunteerconnect.data.model.SignupResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("api/auth/login")
     suspend fun login(@Body loginRequest: LoginRequest): LoginResponse
+
     @POST("api/auth/register")
     suspend fun signup(@Body signupRequest: SignupRequest): SignupResponse
+
+    @GET("api/applications")
+    suspend fun getApplications(): ApplicationResponse
 }
